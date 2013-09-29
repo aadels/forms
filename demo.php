8eaa4382bc3aef205c9cc7bdd0d83ca846455eed
ann adelsberger <adelsbergerann@gmail.com>
2013-09-29 18:58:07 -0400

    syntax error

--- demo.php
+++ demo.php
@@ -1,10 +1,10 @@
 <form method='POST' action ='demo.php'>
 Enter 5 contestants <br>
-<input type='text' name='contestant1'>,br>
-<input type='text' name='contestant2'>,br>
-<input type='text' name='contestant3'>,br>
-<input type='text' name='contestant4'>,br>
-<input type='text' name='contestant5'>,br>
+<input type='text' name='contestant1'>,<br>
+<input type='text' name='contestant2'>,<br>
+<input type='text' name='contestant3'>,<br>
+<input type='text' name='contestant4'>,<br>
+<input type='text' name='contestant5'>,<br>
 <input type='submit' value='Pick a winner!><br>
 <input type='submit'>
 </form>
