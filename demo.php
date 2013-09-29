847163e0ebbabc3dc7c9c561be8657a58beb882b
ann adelsberger <adelsbergerann@gmail.com>
2013-09-29 19:09:14 -0400

    added <pre> section

--- demo.php
+++ demo.php
@@ -1,10 +1,24 @@
-<form method='POST' action ='demo.php'>
-Enter 5 contestants <br>
-<input type='text' name='contestant1'><br>
-<input type='text' name='contestant2'><br>
-<input type='text' name='contestant3'><br>
-<input type='text' name='contestant4'><br>
-<input type='text' name='contestant5'><br>
-<input type='submit' value='Pick a winner!'><br>
-<input type='submit'>
-</form>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Fpor Processing</title>
+	</head>
+	<body>
+		<form method='POST' action ='demo.php'>
+			Enter 5 contestants <br>
+			<input type='text' name='contestant1'><br>
+			<input type='text' name='contestant2'><br>
+			<input type='text' name='contestant3'><br>
+			<input type='text' name='contestant4'><br>
+			<input type='text' name='contestant5'><br>
+			<input type='submit' value='Pick a winner!'><br>
+			<input type='submit'>
+		</form>
+		<pre>
+			</php
+			print_r($_POST);
+			?>
+		</pre>
+		
+	</body>
+</html>
